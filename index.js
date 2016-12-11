ba355fee5b569540d5f888d8216b0fc3a4f64cff
Alessandro Segala <EgoAleSum@users.noreply.github.com>
2016-12-10 19:48:14 -0500

    Separating the CLI from the library. This allows the package to be used as library as well, including it in other applications.

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-#!/usr/bin/env node
-
 'use strict'
 
-// Include the DebugLog function (global method)
-require('./lib/DebugLog')
-
-// Start the application
-const CLI = require('./CLI')
-new CLI()
+// Export the content of StreamUpload
+module.exports = require('./lib/StreamUpload')
