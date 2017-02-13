02dc98e8bf057e566cd458e2901b4e1ea1193c05
Alessandro Segala <EgoAleSum@users.noreply.github.com>
2017-02-12 23:17:01 -0500

    Writing some tests for StreamUpload

--- test/StreamUpload.test.js
+++ test/StreamUpload.test.js
@@ -7,58 +7,120 @@ const TestUtils = require('./_TestUtils')
 const StreamUpload = require('../lib/StreamUpload')
 
 describe('StreamUpload', function() {
-    before(TestUtils.RequireAuth)
+    let storageAccount = {
+        name: null,
+        key: null,
+        sasToken: null
+    }
+
+    // Container name where data is stored
+    let containerName = 'test-' + parseInt(Date.now() / 1000, 10)
+
+    before('ensure credentials are passed', TestUtils.RequireAuth)
+    before('get credentials', TestUtils.GetCredentials(storageAccount, containerName))
     
     it('constructor', function() {
         // Get a readable stream
         let useStream = fs.createReadStream('./assets/unsplash1.jpg')
 
-        // Get credentials from env
-        let storageAccountName = process.env.AZURE_STORAGE_ACCOUNT
-        let storageAccountKey = process.env.AZURE_STORAGE_ACCESS_KEY
+        let credentials = {
+                storageAccountName: storageAccount.name,
+                storageAccountKey: storageAccount.key
+            }
 
         // Initialize object
         assert.doesNotThrow(() => {
-            let upload = new StreamUpload(useStream, '/bak1/test.jpg', {
-                storageAccountName: storageAccountName,
-                storageAccountKey: storageAccountKey
-            })
+            let upload = new StreamUpload(useStream, '/' + containerName + '/test.jpg', credentials)
+        })
+
+        // SAS Token
+        assert.doesNotThrow(() => {
+            let upload = new StreamUpload(useStream, '/' + containerName + '/test.jpg', credentials)
         })
 
         // Should accept anything as input
         assert.doesNotThrow(() => {
-            let upload = new StreamUpload(null, '/bak1/test.jpg', {
-                storageAccountName: storageAccountName,
-                storageAccountKey: storageAccountKey
-            })
+            let upload = new StreamUpload(null, '/' + containerName + '/test.jpg', credentials)
         })
 
         // Should not accept empty or invalid destination blobs
         assert.throws(() => {
-            let upload = new StreamUpload(null, '', {
-                storageAccountName: storageAccountName,
-                storageAccountKey: storageAccountKey
-            })
+            let upload = new StreamUpload(null, '', credentials)
         }, /blob/i)
         assert.throws(() => {
-            let upload = new StreamUpload('/invalid/', '', {
-                storageAccountName: storageAccountName,
-                storageAccountKey: storageAccountKey
-            })
+            let upload = new StreamUpload('/invalid/', '', credentials)
         }, /blob/i)
 
         // Requires authentication data
         assert.throws(() => {
-            let upload = new StreamUpload(null, '/bak1/test.jpg', {
+            let upload = new StreamUpload(null, '/' + containerName + '/test.jpg', {
+                // Name not set
                 storageAccountName: '',
-                storageAccountKey: storageAccountKey
+                storageAccountKey: storageAccount.key
             })
         }, /Storage account name/i)
         assert.throws(() => {
-            let upload = new StreamUpload(null, '/bak1/test.jpg', {
-                storageAccountName: storageAccountName,
-                storageAccountKey: storageAccountKey
+            let upload = new StreamUpload(null, '/' + containerName + '/test.jpg', {
+                // No key or SAS token
+                storageAccountName: storageAccount.name
             })
-        }, /Storage account name/i)
+        }, /storage account key or SAS token/i)
+        assert.throws(() => {
+            let upload = new StreamUpload(null, '/' + containerName + '/test.jpg', {
+                // Both key and SAS token
+                storageAccountName: storageAccount.name,
+                storageAccountKey: storageAccount.key,
+                storageAccountSasToken: storageAccount.sasToken
+            })
+        }, /storage account key or SAS token/i)
+    })
+
+    it('blockSize', function() {
+        // Constants
+        assert.ok(StreamUpload.defaultBlockSize)
+        assert.ok(StreamUpload.maxBlockSize)
+
+        // Initialize object
+        let upload = new StreamUpload(null, '/' + containerName + '/test.jpg', {
+            storageAccountName: storageAccount.name,
+            storageAccountKey: storageAccount.key
+        })
+
+        // Default value
+        assert.equal(upload.blockSize, StreamUpload.defaultBlockSize)
+
+        // Setter test
+        upload.blockSize = 10
+        assert.equal(upload.blockSize, 10)
+
+        // Errors
+        assert.throws(() => { upload.blockSize = 0 }, /block size/i)
+        assert.throws(() => { upload.blockSize = -10 }, /block size/i)
+        assert.throws(() => { upload.blockSize = StreamUpload.maxBlockSize + 1 }, /block size/i)
+        assert.throws(() => { upload.blockSize = 'c' }, /block size/i)
+    })
+
+    it('blocksPerBlob', function() {
+        // Constants
+        assert.ok(StreamUpload.maxBlocksPerBlob)
+
+        // Initialize object
+        let upload = new StreamUpload(null, '/' + containerName + '/test.jpg', {
+            storageAccountName: storageAccount.name,
+            storageAccountKey: storageAccount.key
+        })
+
+        // Default value
+        assert.equal(upload.blocksPerBlob, StreamUpload.maxBlocksPerBlob)
+
+        // Setter test
+        upload.blocksPerBlob = 10
+        assert.equal(upload.blocksPerBlob, 10)
+
+        // Errors
+        assert.throws(() => { upload.blocksPerBlob = 0 }, /blocks per blob/i)
+        assert.throws(() => { upload.blocksPerBlob = -10 }, /blocks per blob/i)
+        assert.throws(() => { upload.blocksPerBlob = StreamUpload.maxBlocksPerBlob + 1 }, /blocks per blob/i)
+        assert.throws(() => { upload.blocksPerBlob = 'c' }, /blocks per blob/i)
     })
 })
--- test/_TestUtils.js
+++ test/_TestUtils.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const azure = require('azure-storage')
+
 module.exports = {
     /**
      * In order to run the tests, we need to have access to an Azure Storage Account.
@@ -12,6 +14,51 @@ module.exports = {
         if(!process.env.AZURE_STORAGE_ACCOUNT || !process.env.AZURE_STORAGE_ACCESS_KEY) {
             console.log('The test suite requires access to an Azure Storage Account. Please ensure that the following environmental variables are set: AZURE_STORAGE_ACCOUNT and AZURE_STORAGE_ACCESS_KEY.')
             this.skip()
+        }
+    },
+
+    /**
+     * Returns a function that stores credentials in the storageAccount param.
+     *
+     * The storageAccount param is modified
+     */
+    GetCredentials: function(storageAccount, containerName) {
+        return function(done) {
+            // Get credentials from env
+            storageAccount.name = process.env.AZURE_STORAGE_ACCOUNT
+            storageAccount.key = process.env.AZURE_STORAGE_ACCESS_KEY
+
+            // Init azure-storage
+            const blobService = azure.createBlobService(storageAccount.name, storageAccount.key)
+
+            // Create a new container
+            blobService.createContainer(containerName, {publicAccessLevel: 'blob'}, (err, result, response) => {
+                if(err) {
+                    throw err
+                }
+
+                // Generate a new SAS token
+                let startDate = new Date()
+                let expiryDate = new Date()
+                startDate.setTime(startDate.getTime() - 1000)
+                expiryDate.setTime(expiryDate.getTime() + 3600*1000)
+
+                let sharedAccessPolicy = {
+                    AccessPolicy: {
+                        Services: azure.Constants.AccountSasConstants.Services.BLOB,
+                        Permissions: azure.Constants.AccountSasConstants.Permissions.WRITE + 
+                            azure.Constants.AccountSasConstants.Permissions.CREATE +
+                            azure.Constants.AccountSasConstants.Permissions.ADD,
+                        Protocols: azure.Constants.AccountSasConstants.Protocols.HTTPSONLY,
+                        Start: startDate,
+                        Expiry: expiryDate
+                    }
+                }
+
+                storageAccount.sasToken = blobService.generateSharedAccessSignature(containerName, null, sharedAccessPolicy)
+
+                done()
+            })
         }
     }
 }
